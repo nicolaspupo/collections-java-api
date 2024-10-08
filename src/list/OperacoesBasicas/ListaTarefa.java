b2d23fd5ae71f48e6449ad6666c23e7cef767645
Nicolas Pupo <nicolaspwpo@gmail.com>
2024-10-08 19:13:09 -0300

    second

---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,47 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+    }
+
+    public int obterNumeroTarefas () {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTarefas());
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.removerTarefa("Tarefa 2");
+
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTarefas());
+
+
+    }
+}
--- src/list/OperacoesBasicas/Tarefa.java
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -1,5 +1,14 @@
 package list.OperacoesBasicas;
 
+@SuppressWarnings("ALL")
 public class Tarefa {
+    private String descricao;
 
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
